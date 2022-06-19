1e108627af56d906bb9fd2dd8cd257d199cc65a9
AlexeyVAV <vitkalovav@gmail.com>
2022-06-19 11:43:18 +0100

    user_control bug fix

--- user_control.sh
+++ user_control.sh
@@ -5,14 +5,16 @@ TIMETABLE_FILE="timetable.csv"
 
 USER_NAME='alex'
 
+WORK_DIR="/root/workplace/user_control"
+
 ### Main code ###
-if [ -f logs/`date +"%Y-%m-%d"`.log ]
+if [ -f $WORK_DIR/logs/`date +"%Y-%m-%d"`.log ]
 then 
     echo "exists"
 else 
     echo "don't"
     usermod --expiredate -1 $USER_NAME
-    touch `logs/date +"%Y-%m-%d"`.log
+    touch $WORK_DIR/logs/`date +"%Y-%m-%d"`.log
 fi
 
 exit 0
